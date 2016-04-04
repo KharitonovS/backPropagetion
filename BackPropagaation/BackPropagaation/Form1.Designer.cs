27113cfd0effbc0f2262dbca3e7b3741e201acbb
Sergey <kharitonov.1994@mail.ru>
2016-04-04 08:57:06 +0400

    Create project

---
+++ BackPropagaation/BackPropagaation/Form1.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace BackPropagaation
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // MainForm
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(633, 358);
+            this.Name = "MainForm";
+            this.Text = "backpropagation";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
