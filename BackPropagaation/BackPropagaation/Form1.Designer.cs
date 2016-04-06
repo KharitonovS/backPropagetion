cfdf8ec92b8527d06651dd53d60a076217a16db7
Sergey <kharitonov.1994@mail.ru>
2016-04-06 23:51:07 +0400

    add gaph interface

--- BackPropagaation/BackPropagaation/Form1.Designer.cs
+++ BackPropagaation/BackPropagaation/Form1.Designer.cs
@@ -28,19 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.pbMain = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbMain)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pbMain
+            // 
+            this.pbMain.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pbMain.Location = new System.Drawing.Point(30, 34);
+            this.pbMain.Name = "pbMain";
+            this.pbMain.Size = new System.Drawing.Size(401, 270);
+            this.pbMain.TabIndex = 0;
+            this.pbMain.TabStop = false;
+            this.pbMain.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pbMain_MouseClick);
             // 
             // MainForm
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.ClientSize = new System.Drawing.Size(633, 358);
+            this.Controls.Add(this.pbMain);
             this.Name = "MainForm";
             this.Text = "backpropagation";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbMain)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pbMain;
     }
 }
 
--- BackPropagaation/BackPropagaation/Form1.cs
+++ BackPropagaation/BackPropagaation/Form1.cs
@@ -12,9 +12,82 @@ namespace BackPropagaation
 {
     public partial class MainForm : Form
     {
+        Bitmap bmp;
+        Graphics graph;
+        Pen pen = new Pen(Color.Black);
+        SolidBrush brush = new SolidBrush(Color.Red);
+        SolidBrush wbrush = new SolidBrush(Color.White);
+
+        int[] X = new int[25];
+
         public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            pbMain.Width = 301;
+            pbMain.Height = 301;
+            bmp = new Bitmap(pbMain.Width, pbMain.Height);
+            graph = Graphics.FromImage(bmp);
+
+            graph.DrawLine(pen, 0, 0, 0, 300);
+            graph.DrawLine(pen, 60, 0, 60, 300);
+            graph.DrawLine(pen, 120, 0, 120, 300);
+            graph.DrawLine(pen, 180, 0, 180, 300);
+            graph.DrawLine(pen, 240, 0, 240, 300);
+            graph.DrawLine(pen, 300, 0, 300, 300);
+
+
+            graph.DrawLine(pen, 0, 0, 300, 0);
+            graph.DrawLine(pen, 0, 60, 300, 60);
+            graph.DrawLine(pen, 0, 120, 300, 120);
+            graph.DrawLine(pen, 0, 180, 300, 180);
+            graph.DrawLine(pen, 0, 240, 300, 240);
+            graph.DrawLine(pen, 0, 300, 300, 300);
+
+            for (int i = 0; i < X.Length; i++)
+            {
+                X[i] = -1;
+            }
+
+            pbMain.Image = bmp;
+        }
+
+        private void pbMain_MouseClick(object sender, MouseEventArgs e)
+        {
+            base.OnMouseClick(e);
+            int x = e.X;
+            int y = e.Y;
+            if (e.Button == MouseButtons.Left)
+                DrawPixel(x, y);
+            if (e.Button == MouseButtons.Right)
+                RedrawPixel(x, y);
+        }
+
+        private void DrawPixel(int x, int y)
+        {
+            int px; // координаты пикселя в сетке
+            int py;
+
+            px = x / 60;
+            py = y / 60;
+            X[py * 5 + px] = 1;
+            graph.FillRectangle(brush, 60 * px + 1, 60 * py + 1, 59, 59);
+            pbMain.Image = bmp;
+        }
+
+        private void RedrawPixel(int x, int y)
+        {
+            int px; // координаты пикселя в сетке
+            int py;
+
+            px = x / 60;
+            py = y / 60;
+            X[px * 5 + py] = -1;
+            graph.FillRectangle(wbrush, 60 * px + 1, 60 * py + 1, 59, 59);
+            pbMain.Image = bmp;
         }
     }
 }
